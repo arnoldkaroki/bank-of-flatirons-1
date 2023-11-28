427cfcb1b6e7dc973d8a8ef03f2b2f140bc503ed
arnoldkaroki <145427394+arnoldkaroki@users.noreply.github.com>
2023-11-28 13:05:04 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
+// App.js
 import React from 'react';
-import BankOfFlatiron from './BankOfFlatiron';
+import TransactionList from './TransactionList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bank Of Flatiron</h1>
+        <h1>Bank Transaction App</h1>
       </header>
       <main>
-        <BankOfFlatiron />
+        <TransactionList />
       </main>
     </div>
   );

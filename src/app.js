af0c3a25ba48db5b16a19671c4cfb1bbfc9a7f6b
arnoldkaroki <145427394+arnoldkaroki@users.noreply.github.com>
2023-11-28 13:07:10 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BankOfFlatiron from './BankOfFlatiron';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bank Of Flatiron</h1>
+      </header>
+      <main>
+        <BankOfFlatiron />
+      </main>
+    </div>
+  );
+}
+
+export default App;
